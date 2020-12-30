513ad6f1ceb888316af2f7938caa50721c1dc991
AC <fantasy1621821@gmail.com>
2020-12-31 02:43:26 +0800

    format

--- Fibonacci/Fibonacci.java
+++ Fibonacci/Fibonacci.java
@@ -6,17 +6,18 @@ import java.util.ArrayList;
  * Fibonacci
  */
 public class Fibonacci {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         fib(12);
     }
-    public static void fib(int num){
+
+    public static void fib(int num) {
         ArrayList<Integer> arr = new ArrayList<Integer>();
         arr.add(1);
         arr.add(1);
-        for(int i  = 2 ; i < num ; i++){
-            arr.add(arr.get(i-1)+arr.get(i-2));
+        for (int i = 2; i < num; i++) {
+            arr.add(arr.get(i - 1) + arr.get(i - 2));
         }
-        
+
         System.out.println(arr);
     }
 }
